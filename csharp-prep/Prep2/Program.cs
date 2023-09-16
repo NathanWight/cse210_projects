adacc4521b5b4f7ccd2e49b639a1ba1de71f9195
Nathan Wight <NathanWight2@gmail.com>
2023-09-15 19:50:31 -0700

    Completed basic

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,7 +3,32 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+    { int percent = 200;
+
+        Console.WriteLine("What was the grade percent?");
+       
+        percent = int.Parse (Console.ReadLine ());
+       string grade = "invalid";
+        
+        if (percent >=90)
+        { 
+            grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+        else if (percent >= 70)
+        {
+            grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+        else{
+            grade = "F";
+        }
+        Console.WriteLine(grade);
     }
 }
